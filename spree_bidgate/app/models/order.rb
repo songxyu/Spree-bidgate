1e75cb82446ac3216354def233736dbcf75bcf68
wikingwei <pubwei@gmail.com>
2013-12-01 17:31:47 +0800

    change some user id column associations

--- spree_bidgate/app/models/order.rb
+++ spree_bidgate/app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ActiveRecord::Base
   attr_accessible :buyer_id, :create_time, :deadline, :deal_date, :deal_price, :price, :price_type, :seller_id, :status
+  
+  belongs_to :user, :class_name => "User", :foreign_key => 'buyer_id'
+  belongs_to :user, :class_name => "User", :foreign_key => 'seller_id'
 end
--- spree_bidgate/app/models/order_price_history.rb
+++ spree_bidgate/app/models/order_price_history.rb
@@ -1,4 +1,5 @@
 class OrderPriceHistory < ActiveRecord::Base
   belongs_to :order
   attr_accessible :bid_time, :price, :seller_id
+  belongs_to :user, :class_name => "User", :foreign_key => 'seller_id'
 end
